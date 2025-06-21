3e4844ab053412f4df5ca7277871c6ca833f52cb
Userpastname <paperpapabuddy@gmail.com>
2025-06-21 18:44:48 -0500

    dasg

--- wwr/challenges/test_chemistry_1.py
+++ wwr/challenges/test_chemistry_1.py
@@ -1,6 +1,6 @@
 # Copyright 2020, Brigham Young University-Idaho. All rights reserved.
 
-from chemistry import make_periodic_table
+from epsort.challenges.chemistry import make_periodic_table
 from pytest import approx
 import pytest
 
